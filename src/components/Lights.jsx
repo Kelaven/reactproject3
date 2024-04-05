9f0e4911ac4d822ccdfc1ad23cb12e67888dc211
Kelaven <kevin.lavenant@live.fr>
2024-04-05 16:55:24 +0200

    rotation tel OK

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -1,19 +1,21 @@
 import { Environment, Lightformer } from "@react-three/drei";
 
+// Ce fichier définit un composant React pour configurer l'éclairage dans une scène 3D utilisant @react-three/fiber et @react-three/drei.
+// Le composant Lights organise et configure plusieurs types d'éclairage pour ajouter de la profondeur, des ombres et des ambiances à la scène 3D.
+
 const Lights = () => {
     return (
-        // group different lights and lightformers. We can use group to organize lights, cameras, meshes, and other objects in the scene.
+        // On peut utiliser group pou rorganiser les lights, cameras, meshes, et autres objets sur la scene.
         <group name="lights">
-            {/**
-       * @description Environment is used to create a background environment for the scene
-       * https://github.com/pmndrs/drei?tab=readme-ov-file#environment
-       */}
+            {/** Le composant group : Il sert à regrouper plusieurs lumières. 
+            Dans three.js et donc dans @react-three/fiber, group est utilisé pour regrouper des objets qui peuvent être manipulés ensemble. Dans ce cas, il s'agit de toutes les lumières de la scène. */}
             <Environment resolution={256}>
+                {/* Le composant Environment : Il est importé de @react-three/drei et est utilisé pour ajouter un éclairage global à la scène, simulant l'éclairage ambiant et les réflexions. 
+                La résolution de 256 indique la qualité des textures d'éclairage indirect utilisées pour simuler cet environnement. */}
                 <group>
-                    {/**
-           * @description Lightformer used to create custom lights with various shapes and properties in a 3D scene.
-           * https://github.com/pmndrs/drei?tab=readme-ov-file#lightformer
-           */}
+                    {/** Les composants Lightformer : Ils créent des lumières personnalisées qui peuvent avoir différentes formes (form="rect" pour rectangulaire ici) et propriétés. 
+                     * Ils sont placés à différentes positions et orientations dans la scène pour simuler l'éclairage d'objets de différentes manières, par exemple, pour créer des effets de lumière directionnelle ou pour mettre en évidence certains éléments de la scène. 
+                     * La propriété intensity contrôle la force de la lumière, position définit où elle se trouve dans la scène, scale ajuste la taille de la source lumineuse, et rotation-y oriente la lumière sur l'axe Y. */}
                     <Lightformer
                         form="rect"
                         intensity={10}
@@ -38,11 +40,9 @@ const Lights = () => {
                 </group>
             </Environment>
 
-            {/**
-       * @description spotLight is used to create a light source positioned at a specific point
-       * in the scene that emits light in a specific direction.
-       * https://threejs.org/docs/#api/en/lights/SpotLight
-       */}
+            {/** Les composants spotLight : Ce sont des sources de lumière ponctuelles de three.js qui éclairent les objets d'un certain angle et direction, donnés par position et angle. 
+            Penumbra ajoute un effet adouci aux bords de l'ombre projetée, et decay détermine la diminution de l'intensité de la lumière avec la distance. 
+            Intensity contrôle la luminosité globale de la lumière et color sa teinte. */}
             <spotLight
                 position={[-2, 10, 5]}
                 angle={0.15}
